36eecaafc8f31770994f17504a73a88390f33a11
Vatsal Namit <vatsalgame@user3-225-3.wireless.utoronto.ca>
2014-02-04 20:28:25 -0500

    Produced a rectangle\!

--- Assignment1/js/SpaceInvaders.js
+++ Assignment1/js/SpaceInvaders.js
@@ -1,15 +1,32 @@
+// The function (class) that initializes the game
+var Board = function(){
+    this.canvas = document.getElementById("mainCanvas");
+    this.context = this.canvas.getContext("2d");
+    
+    this.width = 800;
+    this.height = 600;
 
-// Trial function. Will implement the actual game later
-function drawTriangle() {
-	var canvas = document.getElementById("mainScreen");
-	var context = canvas.getContext("2d");
-	alert("Hello");
-	context.strokeStyle = "blue";
+    this.level = 1;
+};
 
-	context.movetTo(250, 50);
-	context.lineTo(50,250);
-	context.lineTo(450, 250);
-	context.closePath();
+var board = new Board();
 
-	context.stroke();
+var CanonShip = function() {
+    this.x = 380;
+    this.y = 580;
+    this.lives = 3;
+    
+    // this.image = new Image();
+    // img.src = "";
+    board.context.fillStyle = "green"
+    board.context.fillRect(this.x, this.y, 20, 20);
+    
+};
+
+Board.prototype.run = function(){
+    theShip = new CanonShip();   
+};
+
+window.onload = function() {
+	board.run();
 }
